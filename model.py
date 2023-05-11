4f36029845a391964a795dd09dc8c84849293253
Danieli-Costa <danielibibiano2016@gmail.com>
2023-05-11 13:02:31 -0700

    Added comments to explain tables

--- model.py
+++ model.py
@@ -2,11 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-                    #  db.Column("user_id", db.Integer, db.ForeignKey("users.user_id"), primary_key=True),
 
+#Table to connect book and bookshelf
 
 book_users = db.Table("book_users", db.Column("book_id", db.Integer, db.ForeignKey("books.book_id"), primary_key=True),
             db.Column("bookshelf_id", db.Integer, db.ForeignKey("bookshelves.bookshelf_id"), primary_key=True))
+
+
 
 class User(db.Model):
     """ A user """
@@ -19,15 +21,10 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable = False, unique=True)
     password = db.Column(db.String(50), nullable = False)
 
-    # book_users = db.relationship("Bookshelf", backref="bookshelf")
-    # books = db.relationship("Book", secondary=book_users, lazy="subquery",
-    #             backref=db.backref("user", lazy=True))
 
     def __repr__(self):
         return f"<User user_id={self.user_id} email={self.email}>"
 
-# tags = db.relationship('Tag', secondary=tags, lazy='subquery',
-#         backref=db.backref('pages', lazy=True))
 
 
 class Bookshelf(db.Model):
@@ -37,9 +34,8 @@ class Bookshelf(db.Model):
 
     bookshelf_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id")) 
-    # book_id = db.Column(db.Integer, db.ForeignKey("books.book_id")) 
-
-    # book = db.relationship("Book", backref="bookshelves")
+    
+    
     user = db.relationship("User", backref="bookshelves")
 
     books = db.relationship("Book", secondary=book_users, lazy="subquery",
@@ -48,6 +44,7 @@ class Bookshelf(db.Model):
     def __repr__(self):
         return f"<Bookshelf bookshelf_id={self.bookshelf_id} user_id={self.user_id}>"
     
+
 
 class Review(db.Model):
     """ A review on a book """
@@ -65,7 +62,7 @@ class Review(db.Model):
     def __repr__(self):
         return f"<Review review_id={self.review_id} comment={self.comment}>"
 
-# bookshelf3 = Bookshelf(user_id=user3.user_id, book_id=book3.book_id)
+
 
 class Book(db.Model):
     """ A book """
@@ -76,32 +73,12 @@ class Book(db.Model):
     title = db.Column(db.String, nullable = False) 
     author = db.Column(db.String, nullable = False)
 
-    # book_users = db.relationship("Book", backref="book")
-    
-    # bookshelves = db.relationship("Bookshelf", secondary=book_users, lazy="subquery",
-    #             backref=db.backref("books", lazy=True))
     
     def __repr__(self):
         return f"<Book book_id={self.book_id} title={self.title} author={self.author}>"
 
 
-# tags = db.Table('tags',
-#     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-#     db.Column('page_id', db.Integer, db.ForeignKey('page.id'), primary_key=True)
-# )
 
-
-
-
-
-    # book_user_id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    # book_id = db.Column(db.Integer, db.ForeignKey("books.book_id")) 
-    # user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    # bookshelf_id = db.Column(db.Integer, db.ForeignKey("bookshelves.bookshelf_id"))
-
-    # book = db.relationship("Book", backref="book_users")
-    # user = db.relationship("User", backref="book_users")
-    # bookshelf = db.relationship("Bookshelf", backref="book_users")
 
 
 
