60ea6f987d2b3c4adc53a543d01b1d7f7f3b23d1
Danieli-Costa <danielibibiano2016@gmail.com>
2023-05-15 14:10:49 -0700

    Removed api keyes from the file and added to the server since api calls wont be made by this file

--- seed_database.py
+++ seed_database.py
@@ -1,14 +1,7 @@
 """Script to seed database."""
 import server
 import model
-import crud
-import requests
-import json
-import os
 
-# API keys
-NYT_API_KEY = os.environ["NYT_API_KEY"]
-GOOGLE_BOOKS_KEY = os.environ["GOOGLE_BOOKS_KEY"]
 
 # Connect to database and create all tables
 model.connect_to_db(server.app)
